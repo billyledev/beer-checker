e3529689496f55b23e7758e8ed7018794b551fcf
gox <gox.roche@gmail.com>
2024-10-29 19:50:14 +0100

    First proto done

--- public/main.js
+++ public/main.js
@@ -1,40 +1,57 @@
 const imageInputHandler = async (e) => {
+    console.log("imageInputHandler");
+    document.getElementById('result').style.display = "none";
+
     const photo = document.getElementById('photo');
     const photoFile = e.srcElement.files[0];
     
-    document.getElementById('result').innerHTML = "Processing image..."
     const worker = await Tesseract.createWorker('eng');
+    document.getElementById('spinner').style.display = "block";
     const { data: { text } } = await worker.recognize(photoFile);
     await worker.terminate();
-
-    document.getElementById('result').innerHTML = "";
     photo.src = URL.createObjectURL(photoFile);
+
+    document.getElementById('result').style.display = "block";
+    document.getElementById('spinner').style.display = "none";
+    document.getElementById('captureInput').value = null;
     
-    const bieresLines = text.split(/\r?\n/);
-    console.log(bieresLines);
+    const beerNames = text.split(/\r?\n/)
+            .filter(beer => beer !== '')
+            .map(beer => beer.replace(/\s*\d+°?\d*\s*.*$/g, '').replace(/[^\w\s']/g, '').trim());
+    console.log(beerNames);
 
-    const bieres = bieresLines
-        .filter(biere => biere !== '')
-        .map(biere => biere.replace(/\s*\d+°?\d*\s*.*$/g, '')
-                            .replace(/[^\w\s']/g, '')
-                            .trim()
-        );
-    console.log(bieres);
+    for (const beerName of beerNames) {
+        const response = await fetch("http://localhost:3000/rateBeer", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({query: beerName})
+        });
+    
+        const jsonResp = await response.json();
+        console.log(beerName, jsonResp);
+        appendBiersInTable(beerName, jsonResp.data.results.items);
+    }
+}
 
-    const response = await fetch("http://localhost:3000/rateBeer", {
-        method: "POST",
-        mode:  'cors',
-        headers: {
-            'Content-Type': 'application/json',
-            
-        },
-        body: JSON.stringify({
-            query: bieres[0]
-        })
-    });
+const appendBiersInTable = (beerName, beers) => {
+    const tableBody = document.getElementById('biersTable').getElementsByTagName('tbody')[0];
+    if (!beers) {
+        return;
+    }
 
-    console.log(response.status);
-    console.log(await response.json());
+    for (let i = 0; i < beers.length; i++) {
+        let row = "<tr>";
+        row += (i==0 ? `<th rowspan="${beers.length}" scope="row">${beerName}</th>` : "");
+        row += `<td><img class="beerPhoto" src="${beers[i].beer.imageUrl}" /></td>
+            <td>${beers[i].beer.name}</td>
+            <td>${beers[i].beer.style.name}</td>
+            <td>${Math.round(beers[i].beer.overallScore * 100) / 100}</td>
+            <td>${Math.round(beers[i].beer.styleScore * 100) / 100}</td>
+            <td>${beers[i].beer.ratingsCount}</td>
+            <td>${Math.round(beers[i].beer.abv * 100) / 100}</td>
+            </tr>`;
+        tableBody.insertRow().innerHTML = row;
+    }
 }
 
 document.getElementById("captureInput").addEventListener("input", imageInputHandler);
